32c7c381c3b039ffd23fe3c69c141d31c942a280
Thjarkgh <91020110+Thjarkgh@users.noreply.github.com>
2025-02-02 01:50:30 +0100

    Update init.sql

--- deploy/db/init/init.sql
+++ deploy/db/init/init.sql
@@ -1,1 +1,17 @@
-CREATE TABLE `subscriber` ( `email` NVARCHAR(256) NOT NULL, `confirmationSecret` NVARCHAR(256) NOT NULL );
+CREATE TABLE IF NOT EXISTS `subscriber` (
+  `id` bigint not null AUTO_INCREMENT PRIMARY KEY,
+  `email` varchar(256) NOT NULL UNIQUE KEY,
+  `confirmed` bit NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `subscriberSecretPurpose` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `purpose` varchar(128) NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS `subscriberSecret` (
+  `id` bigint not null AUTO_INCREMENT PRIMARY KEY,
+  `subscriberId` bigint NOT NULL REFERENCES `subscriber` ( `id` ),
+  `purposeId` int NOT NULL REFERENCES `subscriberSecretPurpose` ( `id` ),
+  `validUntil` datetime NOT NULL,
+  `secret` char(64) NOT NULL
+);
+ 
