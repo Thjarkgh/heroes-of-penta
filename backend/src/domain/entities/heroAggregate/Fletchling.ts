b83d4dcee384964504fae2d881408b10cb3e32fe
Thjarkgh <thjarkgh@heroesofpenta.com>
2025-02-25 00:05:28 +0100

    hero domain placeholder

---
+++ backend/src/domain/entities/heroAggregate/Fletchling.ts
@@ -0,0 +1,31 @@
+export default class Fletchling {
+  constructor(
+    public readonly id: string,
+    private _xp: number,
+    private _disposition: Map<string, number>
+  ) {}
+
+  addDisposition(disposition: Map<string, number>) {
+    let xp = 0;
+    for (const d of disposition) {
+      if (d[1] < 0 || d[1] > 100) {
+        throw new Error(`Invalid disposition value ${d[1]} for ${d[0]}`);
+      }
+
+      const existing = this._disposition.get(d[0]);
+      if (existing == undefined) {
+        this._disposition.set(d[0], d[1]);
+        xp += d[1];
+      } else {
+        this._disposition.set(d[0], existing + d[1]);
+        xp += d[1];
+      }
+    }
+
+    this._xp += 1; // TODO: should depend on disposition
+  }
+
+  get disposition() {
+    return this._disposition;
+  }
+}
---
+++ backend/src/domain/entities/heroAggregate/Hero.ts
@@ -0,0 +1,6 @@
+export default class Hero {
+  constructor(
+    public readonly id: number
+  ) {}
+  // TODO: the rest (not now)
+}
