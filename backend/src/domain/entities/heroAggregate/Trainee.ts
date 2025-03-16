3c36531cf31b11ad0ecc692f999644d64de799a7
Thjarkgh <thjarkgh@heroesofpenta.com>
2025-03-16 16:13:49 +0100

    improve xp formula

--- backend/src/domain/entities/heroAggregate/Trainee.ts
+++ backend/src/domain/entities/heroAggregate/Trainee.ts
@@ -21,11 +21,19 @@ export default class Trainee {
     }
 
     let xp = 0;
+    let max = 0;
+    let min = 100;
     for (const d of disposition) {
       if (d[1] < 0 || d[1] > 100) {
         throw new Error(`Invalid disposition value ${d[1]} for ${d[0]}`);
       }
 
+      if (d[1] > max) {
+        max = d[1];
+      }
+      if (d[1] < min) {
+        min = d[1];
+      }
       const existing = this._disposition.get(d[0]);
       if (existing == undefined) {
         this._disposition.set(d[0], d[1]);
@@ -37,7 +45,7 @@ export default class Trainee {
     }
 
     this._lastTraining = timestamp;
-    const xpGain = Math.floor((xp / disposition.size) + 100);
+    const xpGain = Math.floor((xp / disposition.size) + 100 + max - min);
     this._xp += xpGain;
     return xpGain;
   }
