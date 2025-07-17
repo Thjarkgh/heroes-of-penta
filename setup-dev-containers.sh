cbba2f2fb5c24125b7294380f8e1263176bfc226
Thjarkgh <thjarkgh@heroesofpenta.com>
2025-07-18 01:22:49 +0200

    on full containers restart, start node 1 as well

--- setup-dev-containers.sh
+++ setup-dev-containers.sh
@@ -1,4 +1,6 @@
 docker compose down
-docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up mysql -d --remove-orphans --build --force-recreate
-docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up phpmyadmin -d --remove-orphans --build --force-recreate
-docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up litespeed -d --remove-orphans --build --force-recreate
+docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up mysql -d --build --force-recreate
+docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up phpmyadmin -d --build --force-recreate
+docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up litespeed -d --build --force-recreate
+docker compose -f docker-compose.yaml -f docker-compose.dev.yaml build heroes-of-penta-lp-node1 --pull --no-cache
+docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up heroes-of-penta-lp-node1 -d
