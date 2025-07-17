9e37499b9f76d759a48bf8bd0821647fa3b32101
Thjarkgh <thjarkgh@heroesofpenta.com>
2025-07-18 01:17:33 +0200

    fix restart commands

--- setup-dev-containers.sh
+++ setup-dev-containers.sh
@@ -1,4 +1,4 @@
 docker compose down
-docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up mysql -d
-docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up phpadmin -d
-docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up litespeed -d
+docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up mysql -d --remove-orphans --build --force-recreate
+docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up phpmyadmin -d --remove-orphans --build --force-recreate
+docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up litespeed -d --remove-orphans --build --force-recreate
